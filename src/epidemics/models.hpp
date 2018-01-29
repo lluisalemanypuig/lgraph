d27f6c7327e4415b27567b92a01946b700cdf360
Lluís Alemany Puig <lluis.alemany.puig@gmail.com>
2018-01-29 19:30:25 +0100

    Added a crandom_generator to the parameters of the models

--- src/epidemics/models.hpp
+++ src/epidemics/models.hpp
@@ -5,6 +5,7 @@
 using namespace std;
 
 /// Custom includes
+#include "src/crandom_generator.hpp"
 #include "src/graph.hpp"
 
 namespace dsa {
@@ -23,32 +24,45 @@ namespace epidemics {
 	All methods follow the same convention for their input parameters:
 	- G: the network over which the infection spreads
 	- p0: initial proportion of infected individuals.
-			0 <= p0 <= 1
+		0 <= p0 <= 1
 	- beta: rate of infection of an individual with a single neighbour.
-			0 <= beta <= 1
+		0 <= beta <= 1
 	- gamma: rate of recovery of an individual with a single neighbour.
-			0 <= gamma <= 1
+		0 <= gamma <= 1
 	- T: maximum number of steps of the simulation
+	- rg: the random generator used to generate real uniform numbers
+		between 0 and 1.
 	
 	Likewise for their output parameters:
 	- n_inf: n_infected[i] contains the amount of infected nodes at step i.
-			 The number of healthy agents at step i can be obtained doing:
-			 n_healthy[i] = G.n_nodes() - n_inf[i]
+		The number of healthy agents at step i can be obtained doing:
+		n_healthy[i] = G.n_nodes() - n_inf[i]
 	*/
+	
+	template<
+		class G = default_random_engine,
+		typename cT = float
+	>
 	void SIR
 	(
 		const graph& G,
 		double p0, double beta, double gamma,
 		size_t T,
+		crandom_generator<> *rg,
 		
 		vector<size_t>& n_inf
 	);
 	
+	template<
+		class G = default_random_engine,
+		typename cT = float
+	>
 	void SIS
 	(
 		const graph& G,
 		double p0, double beta, double gamma,
 		size_t T,
+		crandom_generator<> *rg,
 		
 		vector<size_t>& n_inf
 	);
@@ -58,3 +72,5 @@ namespace epidemics {
 } // -- namespace networks
 } // -- namespace dsa
 
+#include "sir.cpp"
+#include "sis.cpp"
