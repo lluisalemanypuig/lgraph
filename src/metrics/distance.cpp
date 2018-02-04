4864b0bc00f716b9a8ade40b524a48f8184bbab4
Lluís Alemany Puig <lluis.alemany.puig@gmail.com>
2018-02-04 20:54:09 +0100

    Prepared code for distance metrics

---
+++ src/metrics/distance.cpp
@@ -0,0 +1,37 @@
+#include "metrics/distance.hpp"
+
+namespace dsa {
+namespace networks {
+namespace metrics {
+namespace centralities {
+	
+	size_t diameter(const graph& G) {
+		// calculate all geodesic distances gds
+		// call diameter(G, gds)
+	}
+	
+	size_t diameter(const graph& G, const vector<size_t>& ds) {
+	}
+	
+	double mgc(const graph& G) {
+		// calculate all geodesic distances gds
+		// call mgc(G, gds)
+	}
+	
+	double mgc(const graph& G, const vector<size_t>& ds) {
+	}
+	
+	double mcc(const graph& G) {
+		// calculate closeness centrality of each vertex ccs
+		// call mcc(G, ccs)
+	}
+	
+	double mcc(const graph& G, const vector<double>& cc) {
+		
+	}
+	
+} // -- namespace distance
+} // -- namespace metrics
+} // -- namespace networks	
+} // -- namespace dsa
+
