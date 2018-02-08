cae9f538227605106ea2933424fc69a85f6385c4
Lluís Alemany Puig <lluis.alemany.puig@gmail.com>
2018-02-08 12:35:39 +0100

    File with useful definitions and constants

--- src/utils/definitions.hpp
+++ src/utils/definitions.hpp
@@ -1,5 +1,16 @@
-#ifndef DEFINITIONS_HPP
-#define DEFINITIONS_HPP
+#pragma once
 
-#endif // DEFINITIONS_HPP
+/// C++ includes
+#include <limits>
+using namespace std;
 
+namespace dsa {
+namespace utils {
+
+	template<class T>
+	void UNUSED(const T& t) { (void)t; }
+
+	const static size_t inf = numeric_limits<size_t>::max();
+
+} // -- namespace utils
+} // -- namespace dsa
