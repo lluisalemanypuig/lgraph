c8b8ff223032a8a62ecce761cef74ba58a098acc
Lluís Alemany Puig <lluis.alemany.puig@gmail.com>
2018-02-10 16:26:19 +0100

    Added documentation for BFS method

--- src/graph_traversal/traversal.hpp
+++ src/graph_traversal/traversal.hpp
@@ -10,12 +10,62 @@ using namespace std;
 
 /// Custom includes
 #include "data_structures/graph.hpp"
+#include "utils/logger.hpp"
 
 namespace dsa {
 using namespace utils;
 
 namespace traversal {
 
+	/*
+	Generic Breadth-First search algorithm:
+
+	The usual scheme of this algorithm for traversing through a graph from
+	a source node to a target node is as follows:
+
+	BFS(graph, source, target):
+	1.	vis = {false}	// set of |V(graph)| bits set to false
+	2.	Q = empty		// empty queue
+	3.	while Q is not empty do
+	4.		v = Q.front
+	5.		remove Q's front
+	6.		if v == target then terminate
+	7.		else do
+	8.			Nv = neighbourhood of v
+	9.			for each w in Nv do
+	10.				if w not visited before then
+	11.					push w into Q
+	12.					mark w as visited in vis
+	13.				endif
+	14.			endfor
+	15.		endif
+	16.	endwhile
+
+	This algorithm is implemented in a way generic enough so that function:
+	- proces_current() is called before the if line 6 (and after removing
+		the current node from the queue in line 5) that checks the termination
+		condition.
+		This function admits 3 parameters:
+			* the graph being traversed
+			* the node at the front of the queue ('v' in the pseudocode, line 4)
+			* the set of visited nodes, as a vector<bool>
+	- terminate() is used as a termination condition in line 6.
+		This function admits 3 parameters:
+			* the graph being traversed
+			* the node at the front of the queue ('v' in the pseudocode, line 4)
+			* the set of visited nodes, as a vector<bool>
+	- process_neighbour() is called between the for loop in line 9 and the
+		if in line 10, used to perform some operation on each of the neighbours.
+		This function admits 4 parameters:
+			* the graph being traversed
+			* the node at the front of the queue ('v' in the pseudocode, line 4)
+			* the neighbour of 'v' being processed ('w' in the pseudocode, line 9)
+			* the set of visited nodes, as a vector<bool>
+
+	Find examples on how to use this function in the implementation of methods:
+	- size_t distance(const graph& G, node source, node target);
+	- void distance(const graph& G, node source, vector<size_t>& distances);
+	*/
 	void BFS
 	(
 		const graph& G,
