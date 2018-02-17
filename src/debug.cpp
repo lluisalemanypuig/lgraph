61569b541d95b88ce92accd2554707a6fa922b57
Lluís Alemany Puig <lluis.alemany.puig@gmail.com>
2018-02-17 13:16:11 +0100

    Updated main to display info

--- src/debug.cpp
+++ src/debug.cpp
@@ -13,7 +13,7 @@ using namespace std;
 using namespace dsa;
 using namespace traversal;
 
-void print_distance_matrix(const vector<vector<size_t> >& dist) {
+void print_distance_matrix(const vector<vector<size_t> >& dist, const vector<vector<size_t> >& n_paths) {
 	logger<ofstream>& LOG = logger<ofstream>::get_logger();
 	const size_t N = dist.size();
 
@@ -21,33 +21,39 @@ void print_distance_matrix(const vector<vector<size_t> >& dist) {
 	vector<size_t> max_column(N, 0);
 	for (size_t u = 0; u < N; ++u) {
 		for (size_t v = 0; v < N; ++v) {
-			size_t dist_length = std::to_string(dist[v][u]).length();
+			size_t length_dist = std::to_string(dist[v][u]).length();
 			if (dist[v][u] == utils::inf) {
-				dist_length = 2; // use '-1'
+				length_dist = 2; // use '-1'
 			}
 
-			max_column[u] = max(max_column[u], dist_length);
+			size_t n_paths_length = std::to_string(n_paths[v][u]).length();
+
+			// '|' + ' ' + dist_length + ' ' + '(' + length_n_paths + ')'
+			size_t cell_length = 1 + 1 + length_dist + 1 + 1 + n_paths_length + 1;
+
+			max_column[u] = max(max_column[u], cell_length);
 		}
-		max_column[u] = max(max_column[u], std::to_string(u).length());
+		max_column[u] = max(max_column[u], 2 + std::to_string(u).length());
 	}
 
 	LOG.log() << setw(max_node_length) << " " << " ";
 	for (size_t u = 0; u < N; ++u) {
-		LOG.log() << setw(max_column[u]) << u << " ";
+		LOG.log() << left << setw(max_column[u]) << "| " + std::to_string(u) << " ";
 	}
 	LOG.log() << endl;
 
+	setiosflags(ios::left);
 	for (size_t u = 0; u < N; ++u) {
 		LOG.log() << setw(max_node_length) << u << " ";
 		for (size_t v = 0; v < N; ++v) {
 			LOG.log() << setw(max_column[v]);
+
 			if (dist[u][v] == utils::inf) {
-				LOG.log() << -1;
+				LOG.log() << "| -1 (" + std::to_string(n_paths[u][v]) + ")";
 			}
 			else {
-				LOG.log() << dist[u][v];
+				LOG.log() << "| " + std::to_string(dist[u][v]) + " (" + std::to_string(n_paths[u][v]) + ")";
 			}
-
 			LOG.log() << " ";
 		}
 		LOG.log() << endl;
@@ -85,26 +91,21 @@ void deb_distances(const graph& G, size_t source, size_t target) {
 	LOG.log() << endl;
 
 	// vertex-all
-	vector<size_t> ds;
-	vector<size_t> n_paths;
-	distance(G, source, ds, &n_paths);
+	vector<size_t> ds, va_n_paths;
+	distance(G, source, ds, va_n_paths);
 	LOG.log() << "- node to all" << endl;
 	for (size_t i = 0; i < N; ++i) {
 		LOG.log() << "    Distance from " << source << " to " << i << ": ";
 		if (ds[i] == utils::inf) LOG.log() << "-1";
-		else LOG.log() << ds[i];
+		else LOG.log() << ds[i] << " (" << va_n_paths[i] << ")";
 		LOG.log() << endl;
-	}
-	for (size_t i = 0; i < N; ++i) {
-		LOG.log() << "    Number of paths from " << source
-				  << " to " << i << ": " << n_paths[i] << endl;
 	}
 
 	// all-all
-	vector<vector<size_t> > all_ds;
-	distances(G, all_ds);
+	vector<vector<size_t> > all_ds, all_n_paths;
+	distances(G, all_ds, all_n_paths);
 	LOG.log() << "- all to all" << endl;
-	print_distance_matrix(all_ds);
+	print_distance_matrix(all_ds, all_n_paths);
 }
 
 void deb_paths(const graph& G, size_t source, size_t target) {
@@ -164,6 +165,6 @@ int main(int argc, char *argv[]) {
 	}
 
 	deb_distances(G, S, T);
-	deb_paths(G, S, T);
+	//deb_paths(G, S, T);
 }
 
