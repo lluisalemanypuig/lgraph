d2270d01460fae8710d7e0bb9dc4642bbed4b25e
Lluís Alemany Puig <lluis.alemany.puig@gmail.com>
2018-02-21 19:11:42 +0100

    Added conversiong to boolean path
    - to string (and pretty string)
    - to node path

--- src/data_structures/graph_path.cpp
+++ src/data_structures/graph_path.cpp
@@ -27,19 +27,21 @@ void node_path::concatenate(const node_path& p) {
 /// CLASS boolean_node_path
 
 boolean_path::boolean_path() {
-	n_nodes = 0;
 }
 
 boolean_path::~boolean_path() {
 
 }
 
+/// INITIALISERS
+
 void boolean_path::init(size_t n) {
 	nodes_in_path = vector<bool>(n, false);
+	n_nodes = 0;
 }
 
-void boolean_path::init(const node_path &vp) {
-	init(vp.size());
+void boolean_path::init(size_t n, const node_path &vp) {
+	init(n);
 	for (size_t u : vp) {
 		add_node(u);
 	}
@@ -47,7 +49,10 @@ void boolean_path::init(const node_path &vp) {
 
 void boolean_path::clear() {
 	nodes_in_path.clear();
+	n_nodes = 0;
 }
+
+/// MODIFIERS
 
 void boolean_path::add_node(node u) {
 	if (not nodes_in_path[u]) {
@@ -63,16 +68,28 @@ void boolean_path::remove_node(node u) {
 	}
 }
 
+/// OPERATORS
+
 bool boolean_path::operator[] (size_t i) const {
 	return nodes_in_path[i];
+}
+
+/// GETTERS
+
+size_t boolean_path::path_length() const {
+	return n_nodes;
+}
+
+size_t boolean_path::potential_length() const {
+	return nodes_in_path.size();
 }
 
 node boolean_path::next(const graph& G, node prev, node u) const {
 	node next = u;
 	bool found = false;
-	size_t i = 0;
 
 	const neighbourhood& Nu = G.get_neighbours(u);
+	size_t i = 0;
 	while (i < Nu.size() and not found) {
 		node v = Nu[i];
 		if (nodes_in_path[v] and v != prev) {
@@ -83,6 +100,72 @@ node boolean_path::next(const graph& G, node prev, node u) const {
 	}
 
 	return next;
+}
+
+/// CONVERSIONS
+
+node_path boolean_path::to_node_path(const graph& G, node s) const {
+	node_path np;
+	to_node_path(G, s, np);
+	return np;
+}
+
+void boolean_path::to_node_path(const graph& G, node s, node_path& np) const {
+	np = node_path(0);
+	np.push_back(s);
+
+	size_t added_nodes = 1;
+	node prev_node = -1;
+	node current = s;
+	node next_node;
+
+	while ( (next_node = next(G, prev_node, current)) != current ) {
+		np.push_back(next_node);
+		++added_nodes;
+
+		prev_node = current;
+		current = next_node;
+	}
+}
+
+string boolean_path::pretty_string() const {
+	string s;
+	pretty_string(s);
+	return s;
+}
+
+void boolean_path::pretty_string(string& s) const {
+	if (n_nodes > 0) {
+		stringstream ss;
+
+		ss << "0";
+		for (size_t i = 1; i < nodes_in_path.size(); ++i) {
+			ss << " " << i;
+		}
+		ss << "\n";
+		ss << std::to_string(nodes_in_path[0]);
+		for (size_t i = 1; i < nodes_in_path.size(); ++i) {
+			const bool& ni = nodes_in_path[i];
+			ss << " " << setw(std::to_string(i).length()) << (ni ? "1" : "0");
+		}
+
+		s = ss.str();
+	}
+}
+
+string boolean_path::to_string() const {
+	string s;
+	to_string(s);
+	return s;
+}
+
+void boolean_path::to_string(string& s) const {
+	if (n_nodes > 0) {
+		s = std::to_string(nodes_in_path[0]);
+		for (size_t i = 1; i < nodes_in_path.size(); ++i) {
+			s += " " + std::to_string(nodes_in_path[i]);
+		}
+	}
 }
 
 } // -- namespace utils
--- src/data_structures/graph_path.hpp
+++ src/data_structures/graph_path.hpp
@@ -1,6 +1,10 @@
 #pragma once
 
+/// C includes
+#include <assert.h>
+
 /// C++ includes
+#include <sstream>
 #include <vector>
 using namespace std;
 
@@ -53,29 +57,73 @@ class boolean_path {
 		boolean_path();
 		~boolean_path();
 
+		/// INITIALISERS
+
 		// initialises the path to an empty path of n nodes
 		void init(size_t n);
 
-		// initialises the path to a path of vp.size() nodes
+		// initialises the path to a path of n nodes
 		// with the nodes in vp
-		void init(const node_path& vp);
+		void init(size_t n, const node_path& vp);
 
 		// clears the path. Init has to be called to be
 		// able to use it again.
 		void clear();
 
+		/// MODIFIERS
+
 		// adds/remove a node from the path
 		void add_node(node u);
 		void remove_node(node u);
 
+		/// OPERATORS
+
+		inline friend
+		ostream& operator<< (ostream& os, const boolean_path& bp) {
+			os << bp.to_string();
+			return os;
+		}
+
 		// Check if the i-th node is in the path
 		bool operator[] (size_t i) const;
 
-		// returns the node next to 'u' in the path (which is
+		/// GETTERS
+
+		// Returns the number of nodes in the path
+		size_t path_length() const;
+
+		// Returns the maximum number of nodes that
+		// could be in the path. The size used to initialise it.
+		size_t potential_length() const;
+
+		// Returns the node next to 'u' in the path (which is
 		// different from prev). If the only neighbour of 'u'
 		// in the path is 'prev' then the function returns 'u'.
 		// If 'u' is the first node in the path, set 'prev' to '-1'
+		//
+		// This boolean path is assumed to be minimal in the number
+		// of nodes it contains, that is, u cannot have two of its
+		// neighbours in the path.
 		node next(const graph& G, node prev, node u) const;
+
+		/// CONVERSIONS
+
+		// Converts this boolean path into a node_path. The path
+		// is assumed to start at s.
+		//
+		// This boolean path is assumed to be minimal in the number
+		// of nodes it contains, that is, u cannot have two of its
+		// neighbours in the path.
+		node_path to_node_path(const graph& G, node s) const;
+		void to_node_path(const graph& G, node s, node_path& np) const;
+
+		// If the path has some node in it, converts this boolean path into a
+		// formatted string
+		string pretty_string() const;
+		void pretty_string(string& s) const;
+		// simple string
+		string to_string() const;
+		void to_string(string& s) const;
 };
 
 typedef vector<boolean_path> boolean_path_set;
