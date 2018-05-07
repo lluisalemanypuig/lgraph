2d12b0c06debaa75d00ccf6e890504e705bce391
Lluís Alemany Puig <lluis.alemany.puig@gmail.com>
2018-05-07 14:14:21 +0200

    Documentation for definitions

--- src/utils/definitions.hpp
+++ src/utils/definitions.hpp
@@ -3,29 +3,36 @@
 // C++ includes
 #include <cstdint>
 #include <limits>
+#include <vector>
 using namespace std;
 
 namespace lgraph {
 namespace utils {
 
+	/// Typedef for a graph node's index
 	typedef int32_t node;
+	/// Typedef for a list of nodes
 	typedef vector<node> neighbourhood;
+	/// Typedef for a constant iterator through a @ref neighbourhood
 	typedef neighbourhood::const_iterator ncit;
+	/// Typedef for a non-constant iterator through a @ref neighbourhood
 	typedef neighbourhood::iterator nit;
+	/// Typedef for an edge of a graph
 	typedef pair<node, node> edge;
 
-	template<class T> void UNUSED(const T& t) { (void)t; }
+	/// Function to avoid 'Unused parameter' warnings
+	template<class T> inline void UNUSED(const T& t) { (void)t; }
 
-	// maximum values for 'size_t' and 'doubles'
-	// used as infinites
+	/// Constant for maximum value for unsigned integer type
 	const static size_t z_inf = numeric_limits<size_t>::max();
+	/// Constant for maximum value for double-precision floating point type
 	const static double r_inf = numeric_limits<double>::max();
 
-	// shorthand for any maximum value
-	template<typename T> T inf_t() { return numeric_limits<T>::max(); }
+	/// Shorthand for the maximum value of any type
+	template<typename T> inline T inf_t() { return numeric_limits<T>::max(); }
 
-	// default type for 'null' edge weight
-	typedef size_t _new_;
+	/// Typedef for the type of weight in unweighted graphs
+	typedef uint32_t _new_;
 
 } // -- namespace utils
 } // -- namespace lgraph
