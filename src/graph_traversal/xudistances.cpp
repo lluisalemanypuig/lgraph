a60d53b2897bd169446373d7e26c6e4945e36eaf
Lluís Alemany Puig <lluis.alemany.puig@gmail.com>
2018-05-19 13:32:16 +0200

    Reindentation of code

--- src/graph_traversal/xudistances.cpp
+++ src/graph_traversal/xudistances.cpp
@@ -5,221 +5,221 @@ namespace traversal {
 
 	/// VERTEX-VERTEX
 
-		_new_ xudistance(const xxgraph<_new_> *G, node source, node target) {
-			// distances from source to nodes
-			queue<_new_> distances;
-			distances.push(0);
+	_new_ xudistance(const xxgraph<_new_> *G, node source, node target) {
+		// distances from source to nodes
+		queue<_new_> distances;
+		distances.push(0);
 
-			// current distance from source to u
-			_new_ current_dist;
+		// current distance from source to u
+		_new_ current_dist;
 
-			// distance from source to target. Set to infinite
-			// for those targets not reachable from source.
-			_new_ st_dist = utils::inf_t<_new_>();
+		// distance from source to target. Set to infinite
+		// for those targets not reachable from source.
+		_new_ st_dist = utils::inf_t<_new_>();
 
-			// DO terminate when target node is found. BFS guarantees that when this happens
-			// the shortest distance was found
-			bfs_terminate<_new_> terminate =
-			[&target](const xxgraph<_new_> *, node u, const vector<bool>&)
-			{
-				return u == target;
-			};
+		// DO terminate when target node is found. BFS guarantees that when this happens
+		// the shortest distance was found
+		bfs_terminate<_new_> terminate =
+		[&target](const xxgraph<_new_> *, node u, const vector<bool>&)
+		{
+			return u == target;
+		};
 
-			bfs_process_current<_new_> process_current =
-			[&distances, &current_dist, &target, &st_dist](const xxgraph<_new_> *, node u, const vector<bool>&)
-			{
-				current_dist = distances.front();
-				distances.pop();
-				if (u == target) {
-					st_dist = current_dist;
-				}
-			};
+		bfs_process_current<_new_> process_current =
+		[&distances, &current_dist, &target, &st_dist](const xxgraph<_new_> *, node u, const vector<bool>&)
+		{
+			current_dist = distances.front();
+			distances.pop();
+			if (u == target) {
+				st_dist = current_dist;
+			}
+		};
 
-			// function to compute the shortest distance from source to node v
-			bfs_process_neighbour<_new_> process_neighbour =
-			[&distances, &current_dist](const xxgraph<_new_> *, node, node v, const vector<bool>& vis)
-			{
-				if (not vis[v]) {
-					distances.push(current_dist + 1);
-				}
-			};
+		// function to compute the shortest distance from source to node v
+		bfs_process_neighbour<_new_> process_neighbour =
+		[&distances, &current_dist](const xxgraph<_new_> *, node, node v, const vector<bool>& vis)
+		{
+			if (not vis[v]) {
+				distances.push(current_dist + 1);
+			}
+		};
 
-			BFS(G, source, terminate, process_current, process_neighbour);
+		BFS(G, source, terminate, process_current, process_neighbour);
 
-			return st_dist;
+		return st_dist;
+	}
+
+	_new_ xudistance(const xxgraph<_new_> *G, node source, node target, size_t& n_paths) {
+		vector<_new_> distances;
+		vector<size_t> paths;
+		xudistance(G, source, distances, paths);
+		n_paths = paths[target];
+		return distances[target];
+	}
+
+	/// VERTEX-ALL
+
+	void xudistance(const xxgraph<_new_> *G, node source, vector<_new_>& ds) {
+		const size_t N = G->n_nodes();
+
+		// distance from source to all nodes
+		ds = vector<_new_>(N, utils::inf_t<_new_>());
+		ds[source] = 0;
+
+		// do NOT terminate: iterate through all nodes
+		bfs_terminate<_new_> terminate =
+		[](const xxgraph<_new_> *, node, const vector<bool>&)
+		{
+			return false;
+		};
+
+		// don't need to process the currently visited node
+		bfs_process_current<_new_> process_current =
+		[](const xxgraph<_new_> *, node, const vector<bool>&) { };
+
+		// function to compute the shortest distance from source to node v
+		bfs_process_neighbour<_new_> process_neighbour =
+		[&ds](const xxgraph<_new_> *, node u, node v, const vector<bool>&)
+		{
+			size_t d = ds[u] + 1;
+			if (d < ds[v]) {
+				ds[v] = d;
+			}
+		};
+
+		BFS(G, source, terminate, process_current, process_neighbour);
+	}
+
+	void xudistance(const xxgraph<_new_> *G, node source, vector<_new_>& ds, vector<size_t>& n_paths) {
+		const size_t N = G->n_nodes();
+
+		// distance from source to all nodes
+		ds = vector<_new_>(N, utils::inf_t<_new_>());
+		ds[source] = 0;
+		// number of paths between each pair of nodes
+		n_paths = vector<size_t>(N, 0);
+		n_paths[source] = 1;
+
+		// do NOT terminate: iterate through all nodes
+		bfs_terminate<_new_> terminate =
+		[](const xxgraph<_new_> *, node, const vector<bool>&)
+		{
+			return false;
+		};
+
+		// don't need to process the currently visited node
+		bfs_process_current<_new_> process_current =
+		[](const xxgraph<_new_> *, node, const vector<bool>&) { };
+
+		// function to compute the shortest distance from source to node v
+		bfs_process_neighbour<_new_> process_neighbour =
+		[&ds, &n_paths](const xxgraph<_new_> *, node u, node v, const vector<bool>&)
+		{
+			size_t d = ds[u] + 1;
+			if (d < ds[v]) {
+				ds[v] = d;
+				n_paths[v] = n_paths[u];
+			}
+			else if (d == ds[v]) {
+				++n_paths[v];
+			}
+		};
+
+		BFS(G, source, terminate, process_current, process_neighbour);
+	}
+
+	/// ALL-ALL
+
+	void xudistances(const xxgraph<_new_> *G, vector<vector<_new_> >& dist) {
+		const size_t N = G->n_nodes();
+
+		// initialise data
+		dist = vector<vector<_new_> >(N, vector<_new_>(N, utils::inf_t<_new_>()));
+
+		// initialise with edge weights (here always 1)
+		for (size_t u = 0; u < N; ++u) {
+			const neighbourhood& Nu = G->get_neighbours(u);
+			for (size_t v : Nu) {
+				dist[u][v] = 1;
+			}
 		}
 
-		_new_ xudistance(const xxgraph<_new_> *G, node source, node target, size_t& n_paths) {
-			vector<_new_> distances;
-			vector<size_t> paths;
-			xudistance(G, source, distances, paths);
-			n_paths = paths[target];
-			return distances[target];
-		}
+		// find the all-to-all distance (N^3)
+		for (size_t w = 0; w < N; ++w) {
+			// distance from a vertex to itself is 0
+			dist[w][w] = 0;
 
-		/// VERTEX-ALL
-
-		void xudistance(const xxgraph<_new_> *G, node source, vector<_new_>& ds) {
-			const size_t N = G->n_nodes();
-
-			// distance from source to all nodes
-			ds = vector<_new_>(N, utils::inf_t<_new_>());
-			ds[source] = 0;
-
-			// do NOT terminate: iterate through all nodes
-			bfs_terminate<_new_> terminate =
-			[](const xxgraph<_new_> *, node, const vector<bool>&)
-			{
-				return false;
-			};
-
-			// don't need to process the currently visited node
-			bfs_process_current<_new_> process_current =
-			[](const xxgraph<_new_> *, node, const vector<bool>&) { };
-
-			// function to compute the shortest distance from source to node v
-			bfs_process_neighbour<_new_> process_neighbour =
-			[&ds](const xxgraph<_new_> *, node u, node v, const vector<bool>&)
-			{
-				size_t d = ds[u] + 1;
-				if (d < ds[v]) {
-					ds[v] = d;
-				}
-			};
-
-			BFS(G, source, terminate, process_current, process_neighbour);
-		}
-
-		void xudistance(const xxgraph<_new_> *G, node source, vector<_new_>& ds, vector<size_t>& n_paths) {
-			const size_t N = G->n_nodes();
-
-			// distance from source to all nodes
-			ds = vector<_new_>(N, utils::inf_t<_new_>());
-			ds[source] = 0;
-			// number of paths between each pair of nodes
-			n_paths = vector<size_t>(N, 0);
-			n_paths[source] = 1;
-
-			// do NOT terminate: iterate through all nodes
-			bfs_terminate<_new_> terminate =
-			[](const xxgraph<_new_> *, node, const vector<bool>&)
-			{
-				return false;
-			};
-
-			// don't need to process the currently visited node
-			bfs_process_current<_new_> process_current =
-			[](const xxgraph<_new_> *, node, const vector<bool>&) { };
-
-			// function to compute the shortest distance from source to node v
-			bfs_process_neighbour<_new_> process_neighbour =
-			[&ds, &n_paths](const xxgraph<_new_> *, node u, node v, const vector<bool>&)
-			{
-				size_t d = ds[u] + 1;
-				if (d < ds[v]) {
-					ds[v] = d;
-					n_paths[v] = n_paths[u];
-				}
-				else if (d == ds[v]) {
-					++n_paths[v];
-				}
-			};
-
-			BFS(G, source, terminate, process_current, process_neighbour);
-		}
-
-		/// ALL-ALL
-
-		void xudistances(const xxgraph<_new_> *G, vector<vector<_new_> >& dist) {
-			const size_t N = G->n_nodes();
-
-			// initialise data
-			dist = vector<vector<_new_> >(N, vector<_new_>(N, utils::inf_t<_new_>()));
-
-			// initialise with edge weights (here always 1)
 			for (size_t u = 0; u < N; ++u) {
-				const neighbourhood& Nu = G->get_neighbours(u);
-				for (size_t v : Nu) {
-					dist[u][v] = 1;
+				for (size_t v = 0; v < N; ++v) {
+
+					// ignore the cases where:
+					// the path is not moving
+					if (u == v) continue;
+					// the distances are infinite
+					if (dist[v][w] == utils::inf_t<_new_>()) continue;
+					if (dist[w][u] == utils::inf_t<_new_>()) continue;
+
+					size_t d = dist[u][w] + dist[w][v];
+					if (d < dist[u][v]) {
+						dist[u][v] = dist[u][w] + dist[w][v];
+					}
 				}
 			}
+		}
+	}
 
-			// find the all-to-all distance (N^3)
-			for (size_t w = 0; w < N; ++w) {
-				// distance from a vertex to itself is 0
-				dist[w][w] = 0;
+	void xudistances(const xxgraph<_new_> *G, vector<vector<_new_> >& dist, vector<vector<size_t> >& n_paths) {
+		const size_t N = G->n_nodes();
 
-				for (size_t u = 0; u < N; ++u) {
-					for (size_t v = 0; v < N; ++v) {
+		// initialise data
+		dist = vector<vector<_new_> >(N, vector<_new_>(N, utils::inf_t<_new_>()));
+		n_paths = vector<vector<size_t> >(N, vector<size_t>(N, 0));
 
-						// ignore the cases where:
-						// the path is not moving
-						if (u == v) continue;
-						// the distances are infinite
-						if (dist[v][w] == utils::inf_t<_new_>()) continue;
-						if (dist[w][u] == utils::inf_t<_new_>()) continue;
+		// initialise with edge weights (here always 1) the distance and the
+		// number of shortest-paths from u to all its neighbours
+		for (size_t u = 0; u < N; ++u) {
+			const neighbourhood& Nu = G->get_neighbours(u);
+			for (size_t v : Nu) {
+				dist[u][v] = 1;
+				n_paths[u][v] = 1;
+			}
+		}
 
-						size_t d = dist[u][w] + dist[w][v];
-						if (d < dist[u][v]) {
-							dist[u][v] = dist[u][w] + dist[w][v];
+		// find the all-to-all distance (N^3)
+		for (size_t w = 0; w < N; ++w) {
+			// distance from a vertex to itself is 0
+			dist[w][w] = 0;
+			n_paths[w][w] = 1;
+
+			for (size_t u = 0; u < N; ++u) {
+				for (size_t v = 0; v < N; ++v) {
+
+					// ignore the cases where:
+					// the path is not moving
+					if (u == v) continue;
+					// the distances are infinite
+					if (dist[v][w] == utils::inf_t<_new_>()) continue;
+					if (dist[w][u] == utils::inf_t<_new_>()) continue;
+
+					size_t d = dist[u][w] + dist[w][v];
+					if (d < dist[u][v]) {
+						// this is a shorter path than the shortest found so far
+						dist[u][v] = dist[u][w] + dist[w][v];
+
+						if (u != w and w != v) {
+							n_paths[u][v] = n_paths[u][w]*n_paths[w][v];
+						}
+					}
+					else if (d == dist[u][v]) {
+						// this is a path as short as the shortest found so far
+						if (u != w and w != v) {
+							n_paths[u][v] += n_paths[u][w]*n_paths[w][v];
 						}
 					}
 				}
 			}
 		}
-
-		void xudistances(const xxgraph<_new_> *G, vector<vector<_new_> >& dist, vector<vector<size_t> >& n_paths) {
-			const size_t N = G->n_nodes();
-
-			// initialise data
-			dist = vector<vector<_new_> >(N, vector<_new_>(N, utils::inf_t<_new_>()));
-			n_paths = vector<vector<size_t> >(N, vector<size_t>(N, 0));
-
-			// initialise with edge weights (here always 1) the distance and the
-			// number of shortest-paths from u to all its neighbours
-			for (size_t u = 0; u < N; ++u) {
-				const neighbourhood& Nu = G->get_neighbours(u);
-				for (size_t v : Nu) {
-					dist[u][v] = 1;
-					n_paths[u][v] = 1;
-				}
-			}
-
-			// find the all-to-all distance (N^3)
-			for (size_t w = 0; w < N; ++w) {
-				// distance from a vertex to itself is 0
-				dist[w][w] = 0;
-				n_paths[w][w] = 1;
-
-				for (size_t u = 0; u < N; ++u) {
-					for (size_t v = 0; v < N; ++v) {
-
-						// ignore the cases where:
-						// the path is not moving
-						if (u == v) continue;
-						// the distances are infinite
-						if (dist[v][w] == utils::inf_t<_new_>()) continue;
-						if (dist[w][u] == utils::inf_t<_new_>()) continue;
-
-						size_t d = dist[u][w] + dist[w][v];
-						if (d < dist[u][v]) {
-							// this is a shorter path than the shortest found so far
-							dist[u][v] = dist[u][w] + dist[w][v];
-
-							if (u != w and w != v) {
-								n_paths[u][v] = n_paths[u][w]*n_paths[w][v];
-							}
-						}
-						else if (d == dist[u][v]) {
-							// this is a path as short as the shortest found so far
-							if (u != w and w != v) {
-								n_paths[u][v] += n_paths[u][w]*n_paths[w][v];
-							}
-						}
-					}
-				}
-			}
-		}
+	}
 
 } // -- namespace traversal
 } // -- namespace lgraph
