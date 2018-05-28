92f4b6b76c09780677f4676bcf48aabc1b25e83e
Lluís Alemany Puig <lluis.alemany.puig@gmail.com>
2018-05-28 13:36:50 +0200

    Simplified main
    Move the code of the only function called by main to the main

--- examples/path-finding/path-finding.cpp
+++ examples/path-finding/path-finding.cpp
@@ -10,7 +10,7 @@ using namespace std;
 using namespace lgraph;
 using namespace traversal;
 
-int debug_path_finding(int argc, char *argv[]) {
+int main(int argc, char *argv[]) {
 	string file = "none";
 	bool source_set, target_set, weighted;
 	source_set = target_set = weighted = false;
@@ -78,86 +78,5 @@ int debug_path_finding(int argc, char *argv[]) {
 		functions::deb_bpaths(&G, S, T);
 		functions::deb_all_bpaths(&G, S, T);
 	}
-
-	return 0;
-}
-
-void debug_graphs() {
-	uwgraph<float> Gw;
-	uugraph Gu;
-
-	Gw.init(10);
-	Gu.init(10);
-
-	string g, filename, option;
-	cin >> g;
-
-	size_t u, v;
-	float w;
-
-	while (cin >> option and option != "exit") {
-		if (option == "add-edge") {
-			cin >> u >> v;
-			if (g == "weighted") {
-				cin >> w;
-				Gw.add_edge(u, v, w);
-			}
-			else {
-				Gu.add_edge(u, v);
-			}
-		}
-		else if (option == "remove-edge") {
-			cin >> u >> v;
-			if (g == "weighted") {
-				Gw.remove_edge(u, v);
-			}
-			else {
-				Gu.remove_edge(u, v);
-			}
-		}
-		else if (option == "get-weight") {
-			cin >> u >> v;
-			if (g == "weighted") {
-				cout << Gw.edge_weight(u, v) << endl;
-			}
-			else {
-				cout << Gu.edge_weight(u, v) << endl;
-			}
-		}
-		else if (option == "store") {
-			cin >> filename;
-			if (g == "weighted") {
-				Gw.store_in_file(filename);
-			}
-			else {
-				Gu.store_in_file(filename);
-			}
-		}
-		else if (option == "read") {
-			cin >> filename;
-			if (g == "weighted") {
-				Gw.read_from_file(filename);
-			}
-			else {
-				Gu.read_from_file(filename);
-			}
-		}
-		else if (option == "print") {
-			if (g == "weighted") {
-				cout << Gw << endl;
-			}
-			else {
-				cout << Gu << endl;
-			}
-		}
-	}
-
-	Gw.clear();
-	Gu.clear();
-}
-
-int main(int argc, char *argv[]) {
-	//debug_graphs();
-	return debug_path_finding(argc, argv);
 }
 
