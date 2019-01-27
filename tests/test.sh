f2270d86c79a7f314d9717115eae535d58c61868
Lluís Alemany Puig <lluis.alemany.puig@gmail.com>
2019-01-27 12:25:31 +0100

    Extended script
    Allow execution of all tests

--- tests/test.sh
+++ tests/test.sh
@@ -6,6 +6,8 @@
 function show_usage {
 	echo "Options:"
 	echo "    [-h, --help]: show the usage"
+	echo "    --all: tell the script to execute all groups of tests available."
+	echo "        Every new group of tests needs to be added to the list."
 	echo "    --group=g: specify the group 'g' of tests to be performed."
 	echo "        This string is the path to the directory with all the"
 	echo "        input tests. It can not contain the last slash ('/')"
@@ -13,7 +15,7 @@ function show_usage {
 	echo "    --valgrind : use the memory error detector on every input"
 	echo "        test. This option makes the test ignore the output"
 	echo "        produced and errors are only reported when valgrind"
-	echo "        does so." 
+	echo "        does so."
 }
 
 function exe_valgrind {
@@ -58,8 +60,87 @@ function exe_no_valgrind {
 	fi
 }
 
+function exe_group {
+	# make sure that we have something to test
+	if [ -z "$group" ]; then
+		echo -e "\e[1;4;31mError:\e[0m" "Group of tests was not set" 
+		echo "    Use ./test.sh --help to see the usage"
+		exit
+	fi
+
+	# Make sure the output directory exists
+	input_group=inputs/$group
+	output_group=outputs/$group
+	if [ ! -d $input_group ]; then
+		exit
+	fi
+
+	# Prepare execution command. If valgrind is
+	# requested, make the command appropriately.
+	if [ $use_valgrind = 1 ]; then
+		EXE_COMMAND="valgrind -q --leak-check=full ./$EXE_FILE"
+	else
+		EXE_COMMAND="./$EXE_FILE"
+	fi
+
+	# ------------------------------------------------
+	# Prepare filenames for standard and error outputs
+	keys=$(echo $group | tr "/" "\n");
+	id=$(echo $keys | tr ' ' '.')
+	# TEST_OUT and TEST_ERR are temporary files: a test named, for example,
+	# test-0015 will produce a standard output and an error output into
+	# $TEST_OUT and $TEST_ERR. Then, in case of errors, each file will be
+	# moved to a unique file for this test, namely into the files
+	# $TEST_OUT.0015 and $TEST_ERR.0015. In case of valgrind the contents
+	# of $TEST_ERR will be moved to $VALG_ERR.0015.
+	TEST_OUT=.tmp.$id.out
+	TEST_ERR=.tmp.$id.err
+	VALG_ERR=.vgd.$id.err
+
+	progress=$1
+	echo -e "\e[1;1;33mExecuting tests in \e[0m""\e[1;2;33m$input_group\e[0m ($progress)"
+
+	# execute all tests
+	all_test_files=$(ls $input_group)
+	for f in $all_test_files; do
+		if [ "$f" = "summary.ods" ]; then
+			# skip
+			echo -n ""
+		else
+			# retrieve id from input test
+			# e.g.: 'test-0010' -> '0010'
+			INFILE_LENGTH=${#f}
+			ID=${f:5:($INFILE_LENGTH - 4)}
+			
+			echo -en "    ""\e[1;1;34m$f\e[0m"" "
+			PROG_OUT=$($EXE_COMMAND --input $input_group/$f 2> $TEST_ERR)
+			
+			# parse result of execution command differently,
+			# depending on whether we are using valgrind or not.
+			if [ $use_valgrind = 1 ]; then
+				exe_valgrind
+			else
+				exe_no_valgrind
+			fi
+		fi
+	done
+
+	# Remove those unnecessary temporary files.
+	# File $VALG_ERR is never actually used 'as is'.
+	rm -f $TEST_OUT $TEST_ERR
+}
+
 ########################################################################
 # Code starts here
+
+ALL_GROUPS=("unweighted/undirected/paths"		\
+			"unweighted/undirected/distances"	\
+			"unweighted/directed/paths"			\
+			"unweighted/directed/distances"		\
+			"weighted/undirected/paths"			\
+			"weighted/undirected/distances"		\
+			"weighted/directed/paths"			\
+			"weighted/directed/distances")
 
 EXE_FILE="tests-debug/tests"
 # Make sure that executable file exists.
@@ -74,6 +155,8 @@ usage=0
 group=""
 # use valgrind
 use_valgrind=0
+# execute all tests?
+exe_all=0
 
 for i in "$@"; do
 	case $i in
@@ -88,6 +171,10 @@ for i in "$@"; do
 		shift
 		;;
 		
+		--all)
+		exe_all=1
+		shift
+		;;
 		--valgrind)
 		use_valgrind=1
 		shift
@@ -104,69 +191,23 @@ if [ $usage = 1 ]; then
 	show_usage
 	exit
 fi
-# make sure that we have something to test
-if [ -z "$group" ]; then
-	echo -e "\e[1;4;31mError:\e[0m" "Group of tests was not set" 
-	echo "    Use ./test.sh --help to see the usage"
+
+if [ $exe_all = 1 ] && [ ! -z $group ]; then
+	echo -e "\e[1;4;31mError:\e[0m" "Conflicting options '--all' and '--group'"
 	exit
 fi
 
-# Make sure the output directory exists
-input_group=inputs/$group
-output_group=outputs/$group
-if [ ! -d $input_group ]; then
-	exit
-fi
-
-# Prepare execution command. If valgrind is
-# requested, make the command appropriately.
-if [ $use_valgrind = 1 ]; then
-	EXE_COMMAND="valgrind -q --leak-check=full ./$EXE_FILE"
+if [ $exe_all = 1 ]; then
+	echo -e "\e[1;1;36mExecuting all tests\e[0m"
+	n_groups="${#ALL_GROUPS[@]}"
+	ith_test=1
+	for g in "${ALL_GROUPS[@]}"; do
+		group=$g
+		exe_group "$ith_test/$n_groups"
+		ith_test=$(($ith_test + 1))
+	done
 else
-	EXE_COMMAND="./$EXE_FILE"
+	exe_group "1/1"
 fi
 
-# ------------------------------------------------
-# Prepare filenames for standard and error outputs
-keys=$(echo $group | tr "/" "\n");
-id=$(echo $keys | tr ' ' '.')
-# TEST_OUT and TEST_ERR are temporary files: a test named, for example,
-# test-0015 will produce a standard output and an error output into
-# $TEST_OUT and $TEST_ERR. Then, in case of errors, each file will be
-# moved to a unique file for this test, namely into the files
-# $TEST_OUT.0015 and $TEST_ERR.0015. In case of valgrind the contents
-# of $TEST_ERR will be moved to $VALG_ERR.0015.
-TEST_OUT=.tmp.$id.out
-TEST_ERR=.tmp.$id.err
-VALG_ERR=.vgd.$id.err
 
-echo -e "\e[1;1;33mExecuting tests in \e[0m""\e[1;2;33m$input_group\e[0m"
-
-# execute all tests
-all_test_files=$(ls $input_group)
-for f in $all_test_files; do
-	if [ "$f" = "summary.ods" ]; then
-		# skip
-		echo -n ""
-	else
-		# retrieve id from input test
-		# e.g.: 'test-0010' -> '0010'
-		INFILE_LENGTH=${#f}
-		ID=${f:5:($INFILE_LENGTH - 4)}
-		
-		echo -en "    ""\e[1;1;34m$f\e[0m"" "
-		PROG_OUT=$($EXE_COMMAND --input $input_group/$f 2> $TEST_ERR)
-		
-		# parse result of execution command differently,
-		# depending on whether we are using valgrind or not.
-		if [ $use_valgrind = 1 ]; then
-			exe_valgrind
-		else
-			exe_no_valgrind
-		fi
-	fi
-done
-
-# Remove those unnecessary temporary files.
-# File $VALG_ERR is never actually used 'as is'.
-rm -f $TEST_OUT $TEST_ERR
